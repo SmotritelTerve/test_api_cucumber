602b85194189a01777244b6ee4ffb24e6bc24ff5
SmotritelTerve <peacemo@protonmail.ch>
2019-08-29 21:12:46 +0300

    first commit

---
+++ features/addUser.feature
@@ -0,0 +1,9 @@
+Feature: Add user
+
+    Add user to the test API app /api/user
+
+    Scenario: Add a user to /api/user
+        Given the user name "morpheus" and email "morpheus-overlord@mailinator.com" and password "test123"
+        When they are sent in POST request to user endpoint "http://localhost:8000/api/user/"
+        # And then send GET request to users endpoint "http://localhost:8000/api/users/"
+        Then user name "morpheus" should be in the list of users
---
+++ features/step_definitions/addUser_steps.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const { Before, Given, When, Then} = require('cucumber');
+const User = require('../../lib/user');
+const requests = require('../../lib/requests');
+
+const generateRandomPrefix = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+
+
+let user;
+
+Given(/^the user name "([^"]*)" and email "([^"]*)" and password "([^"]*)"$/, function (name, email, password) {
+    user = new User(generateRandomPrefix + name, generateRandomPrefix + email, password);
+  });
+
+When(/^they are sent in POST request to user endpoint "([^"]*)"$/, function (uriUser) {
+  requests.sendPost(uriUser, user);
+  });
+
+When(/^then send GET request to users endpoint "([^"]*)"$/, function (uriUsers) {
+    requests.sendGet(uriUsers);
+  });
+
+Then(/^user name "([^"]*)" should be in the list of users$/, function (name) {
+    assert.equal(requests.verifyUserName(generateRandomPrefix + name), true)
+  });
