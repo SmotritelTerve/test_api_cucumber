ea6a45fc5d06bc23c491c8a323315abe9f9d679f
SmotritelTerve <peacemo@protonmail.ch>
2019-08-30 18:44:21 +0300

    small code cleaned up

--- features/step_definitions/addUser_steps.js
+++ features/step_definitions/addUser_steps.js
@@ -16,11 +16,7 @@ Given(/^the user name "([^"]*)" and email "([^"]*)" and password "([^"]*)"$/, fu
   });
 
 When(/^they are sent in POST request to user endpoint$/, async function () {
-  user.id = await requests.sendPost(user);
-  });
-
-When(/^then send GET request to users endpoint "([^"]*)"$/, function (uriUsers) {
-    requests.sendGet(uriUsers);
+  user.id = await requests.addUser(user);
   });
 
 Then(/^user with email "([^"]*)" should be in the list of users$/, async function (email) {
@@ -28,29 +24,29 @@ Then(/^user with email "([^"]*)" should be in the list of users$/, async functio
     assert.equal(ifExists, true);
   });
 
-  When(/^send GET request to get the user by id$/, async function () {
+When(/^send GET request to get the user by id$/, async function () {
     receivedUser = await requests.getUserById(user.id);
   });
 
-  Then(/^the user with name "([^"]*)" and email "([^"]*)" should be in the response$/, function (name, email) {
+Then(/^the user with name "([^"]*)" and email "([^"]*)" should be in the response$/, function (name, email) {
     assert.equal(receivedUser.name, name);
     assert.equal(receivedUser.email, email);
   });
 
-  When(/^send DELETE request to delete the user by id$/, async function () {
+When(/^send DELETE request to delete the user by id$/, async function () {
     responseMessage = await requests.deleteUserById(user.id);
   });
 
-  Then(/^the response message should be "([^"]*)"$/, async function (message) {
+Then(/^the response message should be "([^"]*)"$/, async function (message) {
     assert.equal(responseMessage, message);
   });
 
-  Then(/^user with email "([^"]*)" should not be in the list of users$/, async function (email) {
+Then(/^user with email "([^"]*)" should not be in the list of users$/, async function (email) {
     ifExists = await requests.verifyUserEmail(email);
     assert.equal(ifExists, false);
   });
 
-  When(/^new user name "([^"]*)" and email "([^"]*)" sent in PATCH by id$/, async function (nameUpdated, emailUpdated) {
+When(/^new user name "([^"]*)" and email "([^"]*)" sent in PATCH by id$/, async function (nameUpdated, emailUpdated) {
     updatedUser = new User(nameUpdated, emailUpdated);
     responseMessage = await requests.updateUserById(user.id, updatedUser);
   });
