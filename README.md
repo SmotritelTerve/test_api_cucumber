b09a62d1d73ed38c314f731086959c1beebc9bac
SmotritelTerve <peacemo@protonmail.ch>
2019-08-30 16:56:45 +0300

    added README.md and a small fix

---
+++ README.md
@@ -0,0 +1,37 @@
+# API testing with JavaScript : Cucumber JS
+
+A test project learn API testing it with Cucumber JS.
+The test API is based on Node JS, Express and SQLite.
+
+## Installation
+
+Clone the project and run the server.
+
+```node
+npm start
+```
+
+## Usage
+
+```node
+npm test
+```
+
+## Endpoints and methods
+GET localhost:8000/api/users
+
+POST localhost:8000/api/user
+
+GET localhost:8000/api/user/:id
+
+PATCH localhost:8000/api/user/:id
+
+DELETE localhost:8000/api/user/:id
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Acknowledgments
+
+The code to create the test server was taken from:
+[developerhowto.com](https://developerhowto.com/2018/12/29/build-a-rest-api-with-node-js-and-express-js/)
--- features/step_definitions/addUser_steps.js
+++ features/step_definitions/addUser_steps.js
@@ -3,14 +3,14 @@ const { Before, Given, When, Then} = require('cucumber');
 const User = require('../../lib/user');
 const requests = require('../../lib/requests');
 
-const generateRandomPrefix = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+const generatedRandomPrefix = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
 
 let user;
 let receivedUser;
 let responseMessage;
 
 Given(/^the user name "([^"]*)" and email "([^"]*)" and password "([^"]*)"$/, function (name, email, password) {
-    user = new User(name, generateRandomPrefix + email, password);
+    user = new User(name, generatedRandomPrefix + email, password);
   });
 
 When(/^they are sent in POST request to user endpoint$/, async function () {
@@ -22,7 +22,7 @@ When(/^then send GET request to users endpoint "([^"]*)"$/, function (uriUsers) 
   });
 
 Then(/^user with email "([^"]*)" should be in the list of users$/, async function (email) {
-    ifExists = await requests.verifyUserEmail(generateRandomPrefix + email);
+    ifExists = await requests.verifyUserEmail(generatedRandomPrefix + email);
     assert.equal(ifExists, true);
   });
 
@@ -32,7 +32,7 @@ Then(/^user with email "([^"]*)" should be in the list of users$/, async functio
 
   Then(/^the user with name "([^"]*)" and email "([^"]*)" should be in the response$/, function (name, email) {
     assert.equal(receivedUser.name, name);
-    assert.equal(receivedUser.email, generateRandomPrefix + email);
+    assert.equal(receivedUser.email, generatedRandomPrefix + email);
   });
 
   When(/^send DELETE request to delete the user by id$/, async function () {
@@ -44,6 +44,6 @@ Then(/^user with email "([^"]*)" should be in the list of users$/, async functio
   });
 
   Then(/^user with email "([^"]*)" should not be in the list of users$/, async function (email) {
-    ifExists = await requests.verifyUserEmail(generateRandomPrefix + email);
+    ifExists = await requests.verifyUserEmail(generatedRandomPrefix + email);
     assert.equal(ifExists, false);
   });
