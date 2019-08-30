608c53d764eb8b51889e7eac1f2f8b1e64208ed4
SmotritelTerve <peacemo@protonmail.ch>
2019-08-30 17:48:32 +0300

    added step to verify updating a user

--- features/addUser.feature
+++ features/addUser.feature
@@ -1,13 +1,17 @@
-Feature: Add, get, delete a user
+Feature: Add, get, update and delete a user
 
-    Add, get, delete auser to the test API app
+    Add a user to the test API app, get, update and then delete the user
 
-    Scenario: Add, get, delete a user to /api/user
+    Scenario: Add, get, update, delete a user
         Given the user name "morpheus" and email "morpheus-overlord@mailinator.com" and password "test123"
         When they are sent in POST request to user endpoint
         Then user with email "morpheus-overlord@mailinator.com" should be in the list of users
         When send GET request to get the user by id
         Then the user with name "morpheus" and email "morpheus-overlord@mailinator.com" should be in the response
+        When new user name "morpheus-updated" and email "morpheus-updated@mailinator.com" sent in PATCH by id
+        Then the response message should be "success"
+        When send GET request to get the user by id
+        Then the user with name "morpheus-updated" and email "morpheus-updated@mailinator.com" should be in the response
         When send DELETE request to delete the user by id
         Then the response message should be "deleted"
         And user with email "morpheus-overlord@mailinator.com" should not be in the list of users
--- features/step_definitions/addUser_steps.js
+++ features/step_definitions/addUser_steps.js
@@ -3,14 +3,16 @@ const { Before, Given, When, Then} = require('cucumber');
 const User = require('../../lib/user');
 const requests = require('../../lib/requests');
 
-const generatedRandomPrefix = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+// use generatedRandomPrefix in the future tests
+// const generatedRandomPrefix = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
 
 let user;
 let receivedUser;
+let updatedUser;
 let responseMessage;
 
 Given(/^the user name "([^"]*)" and email "([^"]*)" and password "([^"]*)"$/, function (name, email, password) {
-    user = new User(name, generatedRandomPrefix + email, password);
+    user = new User(name, email, password);
   });
 
 When(/^they are sent in POST request to user endpoint$/, async function () {
@@ -22,7 +24,7 @@ When(/^then send GET request to users endpoint "([^"]*)"$/, function (uriUsers) 
   });
 
 Then(/^user with email "([^"]*)" should be in the list of users$/, async function (email) {
-    ifExists = await requests.verifyUserEmail(generatedRandomPrefix + email);
+    ifExists = await requests.verifyUserEmail(email);
     assert.equal(ifExists, true);
   });
 
@@ -32,7 +34,7 @@ Then(/^user with email "([^"]*)" should be in the list of users$/, async functio
 
   Then(/^the user with name "([^"]*)" and email "([^"]*)" should be in the response$/, function (name, email) {
     assert.equal(receivedUser.name, name);
-    assert.equal(receivedUser.email, generatedRandomPrefix + email);
+    assert.equal(receivedUser.email, email);
   });
 
   When(/^send DELETE request to delete the user by id$/, async function () {
@@ -44,6 +46,11 @@ Then(/^user with email "([^"]*)" should be in the list of users$/, async functio
   });
 
   Then(/^user with email "([^"]*)" should not be in the list of users$/, async function (email) {
-    ifExists = await requests.verifyUserEmail(generatedRandomPrefix + email);
+    ifExists = await requests.verifyUserEmail(email);
     assert.equal(ifExists, false);
+  });
+
+  When(/^new user name "([^"]*)" and email "([^"]*)" sent in PATCH by id$/, async function (nameUpdated, emailUpdated) {
+    updatedUser = new User(nameUpdated, emailUpdated);
+    responseMessage = await requests.updateUserById(user.id, updatedUser);
   });
